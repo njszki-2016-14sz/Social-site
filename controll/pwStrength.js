3723195658dac63845cb31fe12b46567d5f1685a
Kristóf Kovács <kovacsk55@gmail.com>
2016-10-27 23:25:33 +0200

    Began password slider animation (incomplete)

---
+++ controll/pwStrength.js
@@ -0,0 +1,17 @@
+function anim(){
+	if($(".easy").is(":hidden")){
+		$(".easy").animate({
+            width: 'toggle'
+        });
+	}
+	if(true){
+		$(".medium").animate({
+            width: 'toggle'
+        });
+	}
+	if(true){
+		$(".hard").animate({
+            width: 'toggle'
+        });
+	}
+}
