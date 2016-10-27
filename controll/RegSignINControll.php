0ebe542e1001991531e2b63a6bae83ddc5f23998
fulopdavid <fulopdavid232@gmail.com>
2016-10-27 14:14:40 +0200

    SignIn/Registration Handling
    pretty fakin great

---
+++ controll/RegSignINControll.php
@@ -0,0 +1,30 @@
+<?php
+	include("../model/DB.php");
+	$DB = new DB("SocialSiteDB");//Példányosítás
+	$db = $DB->connection; //Connection elnevezése $db ként
+	
+	//Belépés
+	if(isset($_POST['Login'])){
+		if(isset($_POST['E_mail'])){
+			$email=$_POST['E_mail'];
+			if(isset($_POST['Pw'])){
+				$pwd = md5($_POST['Pw']);
+				$sql = "SELECT * FROM Users WHERE '$email' = UserEmail AND '$pwd' = UserPassword;";
+				print_r ($DB->query($sql));
+				
+				//továbbítani a tömböt és az oldalt a newsfeed oldalra
+			}else{
+				print "Nincs jelszó";
+				//Visszaadni hogy rossz a jelszó
+			}
+		}else{
+			print "Nincs email";
+			//Visszaadni hogy rossz az email
+		}
+	}
+	//Regisztráció
+	if(isset($_POST['Register'])){
+		
+	}
+	
+?>
