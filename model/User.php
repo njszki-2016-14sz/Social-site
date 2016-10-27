d338c5db022619a6220c7ab291b8047cf42fc70e
Kovács Krisztián <kriszonodi@gmail.com>
2016-10-27 14:19:05 +0200

    Added New User Profile setting method, Fixed newsfeed HTML, CSS

--- model/User.php
+++ model/User.php
@@ -2,40 +2,15 @@
 class User
 {
 	function __construct() {
-		$this->email = null;
-		$this->name = "";
-		$this->password = "";
+		$this->firstName = "";
+		$this->lastName = "";
+		$this->userTag = "";
+		$this->birthDay = null;
+		$this->profileBio = "";
+		$this->profilePic = null;
 	}
 	
-	function LogIn($name, $password) {
-		$db = mysql_connect('localhost', 'root', '');
-		if($db) {
-			die('Could not connnect: ' .mysql_error());
-		}
-		
-		$db_selected = mysql_select_db('users', $db);
-		if(!$db_selected) {
-			die('Can\'t use: ' .mysql_error());
-		}
-		
-		mysql_query("SET NAME utf8", $db);
-		$res = mysql_query("SELECT * FROM user;", $db);
-		
-		$record = mysql_fetch_assoc($res);
-		
-		if($record["name"] == $name && $record["password"]==md5($password)) {
-			$this->name = $name;
-			$this->password = $password;
-			return true;
-		}
-		
-		function isLoggedIn() {
-			if($this->name == "MyName") {
-				return true;
-			} else {
-				return false;
-			}
-		}
+	function SetProfile() {
 	}
 }
 ?>
