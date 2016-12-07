d246a5f1d41e787220dc1b14700b9198399bd647
Kovács Krisztián <kriszonodi@gmail.com>
2016-12-07 12:25:37 +0100

    Minor Fixes And Shit

--- view/newsfeed.php
+++ view/newsfeed.php
@@ -65,15 +65,17 @@
 						</div>
 					</div>
 					<div id="usersMessage">
-						<div id="poster">
-							MetalHunterR
-						</div>
-						<div id="message">
-							Test Message Here
-						</div>
-						<div id="postDate">
-							2016-12-07
-						</div>						
+						<?php
+							<div id="poster">
+								MetalHunterR
+							</div>
+							<div id="message">
+								Test Message Here
+							</div>
+							<div id="postDate">
+								2016-12-07
+							</div>							
+						?>					
 					</div>	
 				</div>
 				<div id="ads">
